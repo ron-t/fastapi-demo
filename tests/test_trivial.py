3126b4529d142ddb825fac1a7c546f3234466d52
ron-t <17290359+ron-t@users.noreply.github.com>
2024-05-08 13:17:58 +1200

    add test for root version

--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -1,2 +1,8 @@
-def test_true():
-    assert 1 == 1
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_root_version():
+    response = client.get("/")
+    assert response.text == "v2024-05-08"
